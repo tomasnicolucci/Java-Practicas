e6405e603d480d62de6cb193688b52f7aa448c4d
tomasnicolucci <tomas.nicolucci@gmail.com>
2024-02-22 22:48:18 -0300

    FizzBuzz

--- FizzBuzz/src/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,31 @@
 package fizzbuzz;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class FizzBuzz {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese la cantidad de números aleatorios a generar: ");
+		int n = scanner.nextInt();
+		Integer [] numeros = new Integer[n];
 		
+		for(int i=0; i<n; i++) {
+			numeros[i] = (int)((Math.random() * n) + 1);
+		}
+		System.out.println("El array generado es: " + Arrays.toString(numeros));
+		
+		for(int numero : numeros) {
+			String resultado = "";
+			if((numero%3)==0) {
+				resultado += "Fizz";
+			}
+			if((numero%5==0)) {
+				resultado += "Buzz";
+			}
+			System.out.println(!resultado.isEmpty() ? resultado : numero);
+		}
+		scanner.close();
 	}
-
 }
